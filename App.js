af7200a11f1a20e114970f36d66c5bfc08736501
Łukasz Szcześniak <say.hello@viters.net>
2018-03-28 22:25:52 +0200

    Revert "Implementing boilerplate app"
    This reverts commit 386fa8a

--- App.js
+++ App.js
@@ -1,24 +1,67 @@
-import React, { Component } from 'react';
-import { AppRegistry, Button, View } from 'react-native';
-import { Provider as MobXProvider, observer } from 'mobx-react/native';
+import React from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
-import Navigator from './src/navigator';
+import { BleManager } from "react-native-ble-plx";
 
-import Store from './src/stores/store';
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.manager = new BleManager();
+    this.tap = null;
 
-const store = new Store();
+    this.manager.startDeviceScan(null, null, (error, device) => {
+      if (error) {
+          // Handle error (scanning will be stopped automatically)
+          return
+      }
 
-@observer
-class App extends Component {
+      console.log('found device');
+
+      // or other criteria.
+      if (device.name === 'tap-button') { 
+        console.log('found');
+
+        device.connect().then((tap) => {
+          return tap.discoverAllServicesAndCharacteristics()
+        }).then(tap => {
+          this.tap = tap;
+          console.log('ready');
+        });
+          
+        this.manager.stopDeviceScan();
+      }
+    });
+
+    setTimeout(() => {
+      console.log('stop scan');
+      this.manager.stopDeviceScan();
+    }, 10000);
+  }
+
   render() {
     return (
-        <MobXProvider store={store}>
-          <View style={{ paddingTop: 20 }}>
-            <Navigator />
-          </View>
-        </MobXProvider>
+      <View style={styles.container}>
+        <Text>Hello from Wadowice!</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
+        <Button onPress={this.ping.bind(this)} title="Ping device"/>
+      </View>
     );
+  }
+
+  ping() {
+    if (this.tap) {
+      console.log('sending ping to', this.tap.id);
+      this.manager.writeCharacteristicWithoutResponseForDevice(this.tap.id, 'ffe0', 'ffe1', btoa('1'));
+    }
   }
 }
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
