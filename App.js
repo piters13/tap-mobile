ee449748ff23d7621bf26f3bd05f89172577dcec
Łukasz Szcześniak <say.hello@viters.net>
2018-03-28 22:48:51 +0200

    Add boilerplate store

--- App.js
+++ App.js
@@ -1,72 +1,20 @@
 import React from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import { observer, Provider as MobXProvider } from 'mobx-react/native';
 
-import { BleManager } from "react-native-ble-plx";
-import Store from './stores/Store';
-import { observer } from 'mobx-react/native';
+import Store from './src/stores/store';
 
 const store = new Store();
 
 @observer
 export default class App extends React.Component {
-  constructor() {
-    super();
-    this.manager = new BleManager();
-    this.tap = null;
-
-    this.manager.startDeviceScan(null, null, (error, device) => {
-      if (error) {
-          // Handle error (scanning will be stopped automatically)
-          return
-      }
-
-      console.log('found device');
-
-      // or other criteria.
-      if (device.name === 'tap-button') { 
-        console.log('found');
-
-        device.connect().then((tap) => {
-          return tap.discoverAllServicesAndCharacteristics()
-        }).then(tap => {
-          this.tap = tap;
-          console.log('ready');
-        });
-          
-        this.manager.stopDeviceScan();
-      }
-    });
-
-    setTimeout(() => {
-      console.log('stop scan');
-      this.manager.stopDeviceScan();
-    }, 10000);
-  }
-
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Hello from Wadowice!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-        <Button onPress={this.ping.bind(this)} title="Ping device"/>
-      </View>
+        <MobXProvider store={store}>
+          <View style={{ paddingTop: 20 }}>
+            <Text>Hejo!</Text>
+          </View>
+        </MobXProvider>
     );
   }
-
-  ping() {
-    if (this.tap) {
-      console.log('sending ping to', this.tap.id);
-      this.manager.writeCharacteristicWithoutResponseForDevice(this.tap.id, 'ffe0', 'ffe1', btoa('1'));
-    }
-  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
