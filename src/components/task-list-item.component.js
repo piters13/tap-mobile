08b918efe0b222a517320fbbb9d155270d00c4a6
Łukasz Szcześniak <say.hello@viters.net>
2018-04-19 22:23:27 +0200

    Change font style of badge on task-list-item

--- src/components/task-list-item.component.js
+++ src/components/task-list-item.component.js
@@ -3,11 +3,12 @@ import { StyleSheet, Text, View } from 'react-native'
 import { Badge } from 'react-native-elements'
 import { Styles } from '../constants/styles'
 import { Colors } from '../constants/colors'
+import { randomInt } from '../utils/random'
 
 export class TaskListItem extends React.Component {
   constructor (props) {
     super(props)
-    this.state = {notes: 13, actions: 7}
+    this.state = {notes: randomInt(0, 10), actions: randomInt(0, 40)}
   }
 
   render () {
@@ -22,8 +23,8 @@ export class TaskListItem extends React.Component {
           </Text>
         </View>
         <View>
-          <Badge value={this.state.notes} containerStyle={styles.firstBadgeStyle} />
-          <Badge value={this.state.actions} containerStyle={styles.secondBadgeStyle} />
+          <Badge value={this.state.notes} containerStyle={styles.firstBadgeStyle} textStyle={styles.badgeTextStyle} />
+          <Badge value={this.state.actions} containerStyle={styles.secondBadgeStyle} textStyle={styles.badgeTextStyle} />
         </View>
       </View>
     )
@@ -44,14 +45,22 @@ const styles = StyleSheet.create({
   firstBadgeStyle: {
     backgroundColor: '#3c40c6',
     borderRadius: 6,
-    width: 40,
+    width: 37,
     height: 20,
-    marginBottom: 3
+    marginBottom: 3,
+    paddingLeft: 5,
+    paddingRight: 5
   },
   secondBadgeStyle: {
     backgroundColor: '#2c7dce',
     borderRadius: 6,
-    width: 40,
-    height: 20
+    width: 37,
+    height: 20,
+    paddingLeft: 5,
+    paddingRight: 5
+  },
+  badgeTextStyle: {
+    fontFamily: Styles.fonts.RobotoBlack,
+    fontSize: 12
   }
 })
