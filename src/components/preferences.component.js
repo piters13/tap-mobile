bad876fd36bc3c781cc5c7b6bb0202b837234bf5
Piter13 <piotrek2005@gmail.com>
2018-04-24 12:53:02 +0200

    created time pickers for start and end working time settings

--- src/components/preferences.component.js
+++ src/components/preferences.component.js
@@ -2,25 +2,68 @@ import React from 'react'
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
 import { Colors } from '../constants/colors'
 import { Styles } from '../constants/styles'
+import DateTimePicker from 'react-native-modal-datetime-picker'
 
 export class Preferences extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      isStartTimePickerVisible: false,
+      isEndTimePickerVisible: false,
+      startTime: '16:00',
+      endTime: '18:00'
+    }
+  }
+
   render () {
     return (
       <View style={this.props.style}>
         <Text style={styles.sectionTextStyle}>Preferences</Text>
         <View style={{ height: 1, backgroundColor: '#c9d1d8', width: '100%' }} />
-        <TouchableOpacity style={styles.itemStyle}>
+        <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
           <Text style={styles.itemTextStyle}>Workday start</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.itemStyle}>
+          <TouchableOpacity style={styles.itemStyle} onPress={this.showStartTimePicker}>
+            <Text>{this.state.startTime}</Text>
+            <DateTimePicker
+              isVisible={this.state.isStartTimePickerVisible}
+              onConfirm={this.handleStartTimePicked}
+              onCancel={this.hideDateTimePicker}
+              mode='time'
+            />
+          </TouchableOpacity>
+        </View>
+        <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
           <Text style={styles.itemTextStyle}>Workday end</Text>
-        </TouchableOpacity>
+          <TouchableOpacity style={styles.itemStyle} onPress={this.showEndTimePicker}>
+            <Text>{this.state.endTime}</Text>
+            <DateTimePicker
+              isVisible={this.state.isEndTimePickerVisible}
+              onConfirm={this.handleEndTimePicked}
+              onCancel={this.hideDateTimePicker}
+              mode='time'
+            />
+          </TouchableOpacity>
+        </View>
         <TouchableOpacity style={styles.itemStyle}>
           <Text style={styles.itemTextStyle}>Ping interval</Text>
         </TouchableOpacity>
       </View>
     )
   }
+
+  showStartTimePicker = () => this.setState({ isStartTimePickerVisible: true })
+
+  showEndTimePicker = () => this.setState({ isEndTimePickerVisible: true })
+
+  handleStartTimePicked = (time) => {
+    this.setState({ isStartTimePickerVisible: false, startTime: time.toLocaleTimeString().match(/\d{2}:\d{2}/) })
+  }
+
+  handleEndTimePicked = (time) => {
+    this.setState({ isEndTimePickerVisible: false, endTime: time.toLocaleTimeString().match(/\d{2}:\d{2}/) })
+  }
+
+  hideDateTimePicker = () => this.setState({ isStartTimePickerVisible: false, isEndTimePickerVisible: false })
 }
 
 const styles = StyleSheet.create({
@@ -36,6 +79,7 @@ const styles = StyleSheet.create({
   },
   itemStyle: {
     paddingTop: 10,
-    paddingBottom: 10
+    paddingBottom: 10,
+    paddingRight: 20
   }
 })
--- src/components/profile.component.js
+++ src/components/profile.component.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
 import { inject, observer } from 'mobx-react'
-import { Screens } from '../constants/screens'
+// import { Screens } from '../constants/screens'
 import { Colors } from '../constants/colors'
 import { Styles } from '../constants/styles'
 
