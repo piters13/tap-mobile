eec49b5d8496229d921c6792644cdae4de60f680
Łukasz Szcześniak <say.hello@viters.net>
2018-06-09 12:53:31 +0200

    Add creating action on action store

--- src/stores/actions.store.js
+++ src/stores/actions.store.js
@@ -5,12 +5,20 @@ import { gql } from 'apollo-boost'
 export class ActionsStore {
   @observable actions = []
 
-  @action fetchActions = () => {
+  @action fetchActions () {
     apolloClient.query({
-      query: gql`query { me { actions { type, createdAt } } }`,
-      fetchPolicy: 'network-only'
-    }).then(resp => {
-      this.actions = resp.data.me.actions
+      query: gql`query { me { actions { id, type, task { id } } } }`
+    })
+  }
+
+  @action createAction (type) {
+    apolloClient.mutation({
+      query: gql`mutation createAction($type: Int!) { createAction(action: {type: $type}) { id, type, task { id } } }`,
+      variables: {type}
+    }).then((res) => {
+      if (res.data.action) {
+        this.actions.concat([res.data.action])
+      }
     })
   }
 }
